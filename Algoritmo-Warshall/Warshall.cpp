7a07f7f859e7406d577794030b69e3a5b9a2d87c
Matheus Ernan Reichert <matheus_ernan@outlook.com>
2022-02-01 21:57:26 -0300

    lgtm!

--- Algoritmo-Warshall/Warshall.cpp
+++ Algoritmo-Warshall/Warshall.cpp
@@ -10,9 +10,19 @@ Warshall::Warshall(std::vector<std::vector<int>> matrix, int maximoDeVertices)
 }
 
 void Warshall::fechamentoTransitivo() {
+
+    for (int i = 0; i < this->maximoDeVertices; i++) {
+        for (int j = 0; j < this->maximoDeVertices; j++) {
+            if (this->matrizCaminho[i][j] > 0) {
+                this->matrizCaminho[i][j] = 1;
+            }
+
+        }
+    }
+
     for (int k = 0; k < maximoDeVertices; k++) {
         for (int i = 0; i < maximoDeVertices; i++) {
-            if (matrizCaminho[i][k] == 1) {
+            if (matrizCaminho[i][k] > 0) {
                 for (int j = 0; j < maximoDeVertices; j++) {
                         matrizCaminho[i][j] = matrizCaminho[i][j] | matrizCaminho[k][j];
                         std::cout << "Matriz de acessibilidade usando o Algoritmo de Warshall" << std::endl;
